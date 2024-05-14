299224fa05d1b0e2fa865eecff9983325b79c079
Lucas Jellema <lucasjellema@gmail.com>
2024-05-14 21:47:19 +0200

    add vuetify to Vue 3 application

--- src/main.js
+++ src/main.js
@@ -1,13 +1,23 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 import App from './App.vue'
 
-
 const app = createApp(App)
-
 app.use(createPinia())
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+app.use(vuetify)
 
 app.mount('#app')
